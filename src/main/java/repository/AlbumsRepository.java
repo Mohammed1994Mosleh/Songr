5d87055e11f732236d62035efab7c65d12c097ed
Mohammed Mosleh <85104695+Mohammed1994Mosleh@users.noreply.github.com>
2021-10-30 16:38:37 +0200

    Delete src directory

--- src/main/java/repository/AlbumsRepository.java
+++
@@ -1,7 +0,0 @@
-package repository;
-
-import com.example.songr.models.Album;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AlbumsRepository extends CrudRepository<Album,Integer> {
-}
